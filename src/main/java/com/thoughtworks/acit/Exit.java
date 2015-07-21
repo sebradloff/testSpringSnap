5307d05f0270dcc96781444e020d8a4b655fec1c
Machira <rmachira@thoughtworks.com>
2015-07-21 15:11:41 -0500

    Initial functionality

--- src/main/java/com/thoughtworks/acit/Exit.java
+++ src/main/java/com/thoughtworks/acit/Exit.java
@@ -4,4 +4,10 @@ package com.thoughtworks.acit;
  * Created by machira on Jul/21/15.
  */
 public class Exit {
+    public void exit(){
+        /*
+        I don't really do anything.
+
+         */
+    }
 }
