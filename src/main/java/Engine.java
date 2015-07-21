3dd656e8618c7d063b179a9843fa50ed25196b67
Machira <rmachira@thoughtworks.com>
2015-07-21 09:19:43 -0500

    Adds turn method.

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -2,4 +2,14 @@
  * Created by machira on Jul/21/15.
  */
 public class Engine {
+    /**
+     *
+     */
+
+    public void turn(int speed){
+        /*
+         *
+         */
+        speed=speed+1;
+    }
 }
