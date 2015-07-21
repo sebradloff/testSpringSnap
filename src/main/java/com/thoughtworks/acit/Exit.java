f78324c36472c13ae6353541de746bd377b0992f
Machira <rmachira@thoughtworks.com>
2015-07-21 15:20:43 -0500

    Removes buggy exit

--- src/main/java/com/thoughtworks/acit/Exit.java
+++ src/main/java/com/thoughtworks/acit/Exit.java
@@ -4,10 +4,5 @@ package com.thoughtworks.acit;
  * Created by machira on Jul/21/15.
  */
 public class Exit {
-    public void exit(){
-        /*
-        I don't really do anything.
 
-         */
-    }
 }
