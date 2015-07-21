4f596d8942d1ca602022b0a5ccb2beb97ccadce1
Machira <rmachira@thoughtworks.com>
2015-07-21 09:09:07 -0500

    Adds more functionality to cars

--- src/main/java/com/thoughtworks/acit/Car.java
+++ src/main/java/com/thoughtworks/acit/Car.java
@@ -4,4 +4,13 @@ package com.thoughtworks.acit;
  * Created by machira on Jul/21/15.
  */
 public class Car {
+    /**
+     * It shall go really fast!!!
+     */
+
+    Wheel wheel;
+
+    public String vroomVroom(){
+        return "meh";
+    }
 }
